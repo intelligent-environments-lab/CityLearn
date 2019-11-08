bae89f0e3dcd1f36a8a8faf7f2a60d08da8ad436
Piyush Jain <piyushjain1996@gmail.com>
2019-11-08 06:12:11 -0600

    Fix discritization loss counting

--- single_building_discrete_levels.py
+++ single_building_discrete_levels.py
@@ -16,8 +16,6 @@ logger.addHandler(ch)
 loss_coeff = 0.19/24
 efficiency = 1.0
 
-# TODO: Solve the losses due to discretization. Instead of specifying in action how much to charge/discharge, specify the
-# level you want to reach.
 # TODO: Extend to multiple buildings (assuming we have the same cooling demand pattern for the buildings for a time period).
 # TODO: Execute the optimal policy with the environment to assert the cost we have found.
 
@@ -64,10 +62,12 @@ def run_dp(cooling_pump, cooling_storage, building, **kwargs):
   cost_array = np.full((end_time - start_time + 2, charge_levels, action_levels), np.inf)
   cost_array[end_time+1-start_time] = np.zeros((charge_levels, action_levels))
 
+  clipped_action_val = np.full((end_time - start_time + 2, charge_levels, action_levels), np.inf)
+
   # TODO (Readability): Create numpy array that can be indexed using time_step instead of time_step - start_time
   # cost = lambda t, c, a: cost_array[t-start_time][c][a]
 
-  logger.info("ES capacity {0}\n".format(cooling_storage.capacity))
+  logger.debug("ES capacity {0}\n".format(cooling_storage.capacity))
   # logger.debug("Cooling demand\n{0}\n".format(sim_results['cooling_demand'][start_time:end_time+1]))
   # logger.debug("Outside temps\n{0}\n".format(sim_results['t_out'][start_time:end_time+1]))
 
@@ -79,10 +79,10 @@ def run_dp(cooling_pump, cooling_storage, building, **kwargs):
     e = cooling_pump.get_electric_consumption_cooling(sim_results['cooling_demand'][t])
     elec_no_es.append(e*e)
 
-  logger.debug("Sum of Electricity^2 without ES {0}\n".format(elec_no_es))
+  logger.debug("Cost without ES {0}\n".format(np.sqrt(np.sum(elec_no_es))))
 
   # Store the optimal action sequence
-  optimal_action_sequence = np.zeros((end_time - start_time + 2))
+  # optimal_action_sequence = np.zeros((end_time - start_time + 2))
   optimal_action_val = np.zeros((end_time - start_time + 2))
 
   for time_step in range(end_time, start_time-1, -1):
@@ -113,34 +113,46 @@ def run_dp(cooling_pump, cooling_storage, building, **kwargs):
 
         # If we are discharging more than the required cooling demand it is valid, but it doesn't make sense to check higher
         # discharging actions after this action. So break after this one action.
-        if charge_transfer < 0 and -1 * charge_transfer * cooling_storage.capacity * efficiency > sim_results['cooling_demand'][time_step]:
+        if charge_transfer < 0 and -1 * charge_transfer * cooling_storage.capacity * efficiency >= sim_results['cooling_demand'][time_step]:
           break_after_this_action = True
 
         # Adapted from set_storage_cooling()
         cooling_power_avail = cooling_pump.get_max_cooling_power(t_source_cooling = sim_results['t_out'][time_step]) - sim_results['cooling_demand'][time_step]
         if charge_transfer >= 0:
-          cooling_energy_to_storage = min(cooling_power_avail, charge_transfer*cooling_storage.capacity/efficiency)
+          maybe_cooling_energy_to_storage = min(cooling_power_avail, charge_transfer*cooling_storage.capacity/efficiency)
         else:
-          cooling_energy_to_storage = max(-sim_results['cooling_demand'][time_step], charge_transfer*cooling_storage.capacity*efficiency)
+          maybe_cooling_energy_to_storage = max(-sim_results['cooling_demand'][time_step], charge_transfer*cooling_storage.capacity*efficiency)
 
-        cooling_energy_drawn_from_heat_pump = cooling_energy_to_storage + sim_results['cooling_demand'][time_step]
-
-        elec_demand_cooling = cooling_pump.get_electric_consumption_cooling(cooling_supply = cooling_energy_drawn_from_heat_pump)
-
-        if cooling_energy_to_storage >= 0:
-          next_charge_on_es = charge_on_es + cooling_energy_to_storage*efficiency/cooling_storage.capacity
+        if maybe_cooling_energy_to_storage >= 0:
+          maybe_next_charge_on_es = charge_on_es + maybe_cooling_energy_to_storage*efficiency/cooling_storage.capacity
         else:
-          next_charge_on_es = charge_on_es + (cooling_energy_to_storage/efficiency)/cooling_storage.capacity
+          maybe_next_charge_on_es = charge_on_es + (maybe_cooling_energy_to_storage/efficiency)/cooling_storage.capacity
 
         # Note that we are getting the closest lower charge level from next_charge value, this will result in some losses.
-        next_charge_level = get_level(0., 1., next_charge_on_es, charge_levels)
-        next_charge_floor = get_val(0., 1., next_charge_level, charge_levels)
+        next_charge_level = get_level(0., 1., maybe_next_charge_on_es, charge_levels)
+        next_charge = get_val(0., 1., next_charge_level, charge_levels)
+
+        cooling_energy_to_storage = (next_charge - charge_on_es)*cooling_storage.capacity*efficiency
+
+        cooling_energy_drawn_from_heat_pump = cooling_energy_to_storage + sim_results['cooling_demand'][time_step]
+        elec_demand_cooling = cooling_pump.get_electric_consumption_cooling(cooling_supply = cooling_energy_drawn_from_heat_pump)
 
         # J is used at places to denote energy instead of charge value.
-        logger.debug("Cooling demand {0:.2f}; Maybe power avail {1:.2f}; To ES {2:.2f} J, {3:.2f} -> {4:.2f} -> {5:.2f}; From pump {6:.2f}; Elec^2 {7:.2f}; COP {8:.2f}".format(sim_results['cooling_demand'][time_step],
-          cooling_power_avail, cooling_energy_to_storage, charge_on_es, next_charge_on_es, next_charge_floor, cooling_energy_drawn_from_heat_pump, elec_demand_cooling*elec_demand_cooling,
-          cooling_pump.cop_cooling))
+        logger.debug("Cooling demand {0:.2f}; \
+          Maybe power avail {1:.2f}; \
+          To ES {2:.2f} J -> {3:.2f} J, {4:.3f} -> {5:.3f} -> {6:.3f}; \
+          From pump {7:.2f}; \
+          Elec^2 {8:.2f}; \
+          COP {9:.2f}".format(
+            sim_results['cooling_demand'][time_step],
+            cooling_power_avail,
+            maybe_cooling_energy_to_storage, cooling_energy_to_storage,
+            charge_on_es, maybe_next_charge_on_es, next_charge,
+            cooling_energy_drawn_from_heat_pump,
+            elec_demand_cooling*elec_demand_cooling,
+            cooling_pump.cop_cooling))
 
+        clipped_action_val[time_step-start_time][charge_level][action] = next_charge - charge_on_es
         #logger.debug("Minimum elec energy in step {0}, charge {1} is {2}".format(time_step+1, next_charge_level, min(cost[time_step+1][next_charge_level])))
         cost_array[time_step-start_time][charge_level][action] = elec_demand_cooling*elec_demand_cooling + min(cost_array[time_step+1-start_time][next_charge_level])
         # logger.debug("\tMin sum of E^2 on this route {0:.2f}".format(cost_array[time_step-start_time][charge_level][action]))
@@ -153,20 +165,26 @@ def run_dp(cooling_pump, cooling_storage, building, **kwargs):
   for time_step in range(start_time, end_time+1):
     curr_charge = get_val(0., 1., charge_crwl, charge_levels)
     curr_charge_after_loss = get_val(0., 1., charge_crwl, charge_levels) * (1-loss_coeff)
-    optimal_action_sequence[time_step-start_time] = np.argmin(cost_array[time_step-start_time][charge_crwl])
-    optimal_action_val[time_step-start_time] = get_val(action_min, action_max,
-      optimal_action_sequence[time_step-start_time], action_levels)
+    optimal_action_level = np.argmin(cost_array[time_step-start_time][charge_crwl])
+    optimal_action_val[time_step-start_time] = \
+      clipped_action_val[time_step-start_time][charge_crwl][optimal_action_level]
 
-    next_charge = get_val(-1, 1, optimal_action_sequence[time_step-start_time], action_levels) + curr_charge_after_loss
+    next_charge = optimal_action_val[time_step-start_time] + curr_charge_after_loss
     next_charge_floor = get_val(0., 1., get_level(0., 1., next_charge, charge_levels), charge_levels)
 
-    logger.debug("Optimal action seq {0}".format(optimal_action_sequence[time_step-start_time]))
-    logger.debug("{0:.2f}: {1:.2f} -> {2:.2f} -> {3:.2f} -> {4:.2f}; {5:.2f}".format(time_step, curr_charge, curr_charge_after_loss,
+    # logger.debug("Optimal action seq {0}".format(optimal_action_sequence[time_step-start_time]))
+    logger.debug("{0:.2f}: {1:.2f} -> {2:.2f} -> +/- {3:.2f} -> {4:.2f} -> {5:.2f}; {6:.2f}".format(time_step,
+      curr_charge, curr_charge_after_loss,
+      optimal_action_val[time_step-start_time],
       next_charge, next_charge_floor,
-      cost_array[time_step-start_time][charge_crwl][int(optimal_action_sequence[time_step-start_time])]))
+      cost_array[time_step-start_time][charge_crwl][optimal_action_level]))
     charge_crwl = get_level(0., 1., next_charge, charge_levels)
 
-  return np.sqrt(min(cost_array[0][0])), optimal_action_val
+  return optimal_action_val
+
+def reset_all(entities):
+  for entity in entities.values():
+    entity.reset()
 
 def get_cost_of_building(building_uid, **kwargs):
   '''
@@ -197,7 +215,29 @@ def get_cost_of_building(building_uid, **kwargs):
   env = CityLearn(demand_file, weather_file, buildings = buildings, time_resolution = 1,
     simulation_period = (kwargs["start_time"]-1, kwargs["end_time"]))
 
-  cost_from_dp, optimal_action_val = \
+  #RULE-BASED CONTROLLER (Stores energy at night and releases it during the day)
+  from agent import RBC_Agent
+
+  #Instantiatiing the control agent(s)
+  agents = RBC_Agent()
+
+  state = env.reset()
+  done = False
+  while not done:
+      action = agents.select_action(state)
+      next_state, rewards, done, _ = env.step(action)
+      state = next_state
+  cost_rbc = env.cost()
+  print("Hand coded policy cost {0}".format(cost_rbc))
+
+  env.reset()
+  reset_all(heat_pump)
+  reset_all(heat_tank)
+  reset_all(cooling_tank)
+  for building in buildings:
+    building.reset()
+
+  optimal_action_val = \
     run_dp(heat_pump[buildings[-1].buildingId], cooling_tank[buildings[-1].buildingId], buildings[-1], **kwargs)
 
   env.reset()
@@ -206,11 +246,10 @@ def get_cost_of_building(building_uid, **kwargs):
   while not done:
       _, rewards, done, _ = env.step([[optimal_action_val[time_step]] for i in range(len(building_ids))])
       time_step += 1
-  cost_via_env = env.cost()
+  cost_via_dp = env.cost()
 
-  logger.info("Cost via env {0}, Cost claimed by dp {1}".format(cost_via_env, cost_from_dp))
-  assert(cost_via_env == cost_from_dp)
-  return cost_via_env
+  logger.info("Cost via dp {0}\n".format(cost_via_dp))
+  return cost_via_dp
 
 
 import argparse
@@ -237,5 +276,3 @@ args = parser.parse_args()
 cost = get_cost_of_building(args.building_uid, start_time=args.start_time, end_time=args.end_time,
   action_levels=args.action_levels, min_action_val=args.min_action_val, max_action_val=args.max_action_val,
   charge_levels=args.action_levels, min_charge_val=args.min_action_val, max_charge_val=args.max_action_val)
-
-logger.info("Cost -> {0}".format(cost))
