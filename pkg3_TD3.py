4d34936b77573e651473f3c2089485e33f27cfbd
Kacper Twardowski <kanexer@gmail.com>
2020-06-04 00:48:30 +0100

    PKG3 Monitor + Saving

--- pkg3_TD3.py
+++ pkg3_TD3.py
@@ -2,9 +2,9 @@
 # coding: utf-8
 
 """
-Implementation of Soft Actor Critic (SAC) network
+Implementation of Twin Delayed DDPG (TD3) network
 using PyTorch and Stable Baselines 3.
-See https://stable-baselines3.readthedocs.io/en/master/modules/sac.html for algorithm details.
+See https://stable-baselines3.readthedocs.io/en/master/modules/td3.html for algorithm details.
 
 @author: Kacper Twardowski 2020 (kanexer@gmail.com) 
 
@@ -14,22 +14,22 @@ Project for CityLearn Competition
 import torch
 import numpy as np
 import gym
-from stable_baselines3.sac.policies import MlpPolicy as MlpPolicy_SAC
-from stable_baselines3 import SAC
+from stable_baselines3.td3.policies import MlpPolicy as MlpPolicy_TD3
+from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
+from stable_baselines3 import TD3
 from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common import make_vec_env, results_plotter
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.results_plotter import plot_results
-from stable_baselines3.common.vec_env.base_vec_env import VecEnv
 from citylearn import  CityLearn
 from pkg3_utils import available_cpu_count as cpu_cores, SaveOnBestTrainingRewardCallback
 import matplotlib.pyplot as plt
 from pathlib import Path
-import warnings
 import time, sys, os
 import pprint as pp
 
 # Ignore the float32 bound precision warning
+import warnings
 warnings.simplefilter("ignore", UserWarning)
 
 # Central agent controlling the buildings using the OpenAI Stable Baselines
@@ -46,7 +46,7 @@ env = CityLearn(data_path, building_attributes, weather_file, solar_profile, bui
                 cost_function = objective_function, central_agent = True, verbose = 1)
 
 # Store the weights and scores in a new directory
-parent_dir = "alg/sac_{}/".format(time.strftime("%Y%m%d-%H%M%S")) # apprends the timedate
+parent_dir = "alg/td3_{}/".format(time.strftime("%Y%m%d-%H%M%S")) # apprends the timedate
 os.makedirs(parent_dir, exist_ok=True)
 
 # Create log dir
@@ -59,14 +59,24 @@ icount = int(sys.argv[1]) if sys.argv is not None else 10
 log_interval = 1
 check_interval = 1
 
+# the noise objects for DDPG
+_, actions_spaces = env.get_state_action_spaces()
+
+n_actions = 0
+for action in actions_spaces:
+    n_actions += action.shape[-1]
+
 # Make VecEnv + Wrap in Monitor
 env = Monitor(env, filename=log_dir)
 callback = SaveOnBestTrainingRewardCallback(check_freq=check_interval*interval, log_dir=log_dir)
 
-model = SAC(MlpPolicy_SAC, env, verbose=1, learning_rate=0.001, gamma=0.99, batch_size=1024, learning_starts=interval-1, tensorboard_log=parent_dir+"tensorboard/")
+param_noise = None
+action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=float(0.5) * np.ones(n_actions))
+
+model = TD3(MlpPolicy_TD3, env, verbose=0, action_noise=action_noise, learning_starts=interval, tensorboard_log=parent_dir+"tensorboard/")
 print()
 
-model.learn(total_timesteps=interval*icount, log_interval=log_interval, tb_log_name="SAC_{}".format(time.strftime("%Y%m%d")), callback=callback)
+model.learn(total_timesteps=interval*icount, log_interval=log_interval, tb_log_name="TD3_{}".format(time.strftime("%Y%m%d")), callback=callback)
 
 obs = env.reset()
 dones = False
@@ -78,9 +88,6 @@ while dones==False:
 
 env.close()
 
-print()
-pp.pprint(env.cost())
-
 # Plot the reward graph
-plot_results([log_dir], interval*icount, results_plotter.X_TIMESTEPS, "SAC CityLearn")
+plot_results([log_dir], interval*icount, results_plotter.X_TIMESTEPS, "TD3 CityLearn")
 plt.savefig(log_dir+"/rewards.pdf")
---
+++ pkg3_utils.py
@@ -0,0 +1,172 @@
+import os
+import re
+import subprocess
+import gym
+from stable_baselines3.common import results_plotter
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.results_plotter import load_results, ts2xy, plot_results
+from stable_baselines3.common.callbacks import BaseCallback
+import numpy as np
+
+# PKG3 Util methods
+
+# Ref: https://stackoverflow.com/a/1006301
+def available_cpu_count():
+    """ Number of available virtual or physical CPUs on this system, i.e.
+    user/real as output by time(1) when called with an optimally scaling
+    userspace-only program"""
+
+    # cpuset
+    # cpuset may restrict the number of *available* processors
+    try:
+        m = re.search(r'(?m)^Cpus_allowed:\s*(.*)$',
+                      open('/proc/self/status').read())
+        if m:
+            res = bin(int(m.group(1).replace(',', ''), 16)).count('1')
+            if res > 0:
+                return res
+    except IOError:
+        pass
+
+    # Python 2.6+
+    try:
+        import multiprocessing
+        return multiprocessing.cpu_count()
+    except (ImportError, NotImplementedError):
+        pass
+
+    # https://github.com/giampaolo/psutil
+    try:
+        import psutil
+        return psutil.cpu_count()   # psutil.NUM_CPUS on old versions
+    except (ImportError, AttributeError):
+        pass
+
+    # POSIX
+    try:
+        res = int(os.sysconf('SC_NPROCESSORS_ONLN'))
+
+        if res > 0:
+            return res
+    except (AttributeError, ValueError):
+        pass
+
+    # Windows
+    try:
+        res = int(os.environ['NUMBER_OF_PROCESSORS'])
+
+        if res > 0:
+            return res
+    except (KeyError, ValueError):
+        pass
+
+    # jython
+    try:
+        from java.lang import Runtime
+        runtime = Runtime.getRuntime()
+        res = runtime.availableProcessors()
+        if res > 0:
+            return res
+    except ImportError:
+        pass
+
+    # BSD
+    try:
+        sysctl = subprocess.Popen(['sysctl', '-n', 'hw.ncpu'],
+                                  stdout=subprocess.PIPE)
+        scStdout = sysctl.communicate()[0]
+        res = int(scStdout)
+
+        if res > 0:
+            return res
+    except (OSError, ValueError):
+        pass
+
+    # Linux
+    try:
+        res = open('/proc/cpuinfo').read().count('processor\t:')
+
+        if res > 0:
+            return res
+    except IOError:
+        pass
+
+    # Solaris
+    try:
+        pseudoDevices = os.listdir('/devices/pseudo/')
+        res = 0
+        for pd in pseudoDevices:
+            if re.match(r'^cpuid@[0-9]+$', pd):
+                res += 1
+
+        if res > 0:
+            return res
+    except OSError:
+        pass
+
+    # Other UNIXes (heuristic)
+    try:
+        try:
+            dmesg = open('/var/run/dmesg.boot').read()
+        except IOError:
+            dmesgProcess = subprocess.Popen(['dmesg'], stdout=subprocess.PIPE)
+            dmesg = dmesgProcess.communicate()[0]
+
+        res = 0
+        while '\ncpu' + str(res) + ':' in dmesg:
+            res += 1
+
+        if res > 0:
+            return res
+    except OSError:
+        pass
+
+    raise Exception('Can not determine number of CPUs on this system')
+
+# Callback class for saving the best reward episode
+# Ref: https://stable-baselines3.readthedocs.io/en/master/guide/examples.html#using-callback-monitoring-training
+class SaveOnBestTrainingRewardCallback(BaseCallback):
+    """
+    Callback for saving a model (the check is done every ``check_freq`` steps)
+    based on the training reward (in practice, we recommend using ``EvalCallback``).
+
+    :param check_freq: (int)
+    :param log_dir: (str) Path to the folder where the model will be saved.
+      It must contains the file created by the ``Monitor`` wrapper.
+    :param verbose: (int)
+    """
+    def __init__(self, check_freq: int, log_dir: str, verbose=1):
+        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, 'best_model')
+        self.best_mean_reward = -np.inf
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self.check_freq == 0:
+
+            # Retrieve training reward
+            x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+            if len(x) > 0:
+                # Mean training reward over the last 100 episodes
+                mean_reward = np.mean(y[-100:])
+                if self.verbose > 0:
+                    print("Num timesteps: {}".format(self.num_timesteps))
+                    print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(self.best_mean_reward, mean_reward))
+
+                # New best model, you could save the agent here
+                if mean_reward > self.best_mean_reward:
+                    self.best_mean_reward = mean_reward
+                    # Example for saving best model
+                    if self.verbose > 0:
+                        print("Saving new best model to {}".format(self.save_path))
+                    self.model.save(self.save_path)
+            
+            print()
+
+        return True
