da57153c7ae60449c675de4f8ecf4ac683bb0536
Bo Liu <bliu0201@gmail.com>
2021-07-14 09:56:21 -0500

    update

--- comix/src/run.py
+++ comix/src/run.py
@@ -38,7 +38,8 @@ def run(_run, _config, _log, pymongo_client):
     _log.info("\n\n" + experiment_params + "\n")
 
     # configure tensorboard logger
-    unique_token = "{}__{}".format(args.name, datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+    #unique_token = "{}__{}".format(args.name, datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+    unique_token = "{}__{}".format(args.name, args.seed)
     args.unique_token = unique_token
     if args.use_tensorboard:
         tb_logs_direc = os.path.join(dirname(dirname(abspath(__file__))), "results", "tb_logs")
--- comix/test.sh
+++ comix/test.sh
@@ -1,15 +1,9 @@
-currdir=/home/users/bliu/research/CityLearn/comix
+currdir=$(dirname "$0")
 
-export CUDA_VISIBLE_DEVICES=0 && python src/main.py --config=comix-naf --env-config=citylearn with t_max=34400 seed=1 evaluate=True test_nepisode=10 runner=episode batch_size_run=1 batch_size=1 checkpoint_path=$currdir/results/models/comix-naf__2021-07-10_02-52-01/ save_replay=True > logs/comix-naf-test.log 2>&1 &
+names=("comix", "covdn-naf" "iql-cem" "iql-naf" "maddpg")
+gpus=(1 2 3 5 7)
 
-export CUDA_VISIBLE_DEVICES=1 && python src/main.py --config=covdn --env-config=citylearn with t_max=34400 seed=1 evaluate=True test_nepisode=10 runner=episode batch_size_run=1 batch_size=1 checkpoint_path=$currdir/results/models/covdn__2021-07-10_02-52-01/ save_replay=True > logs/covdn-test.log 2>&1 &
-
-export CUDA_VISIBLE_DEVICES=2 && python src/main.py --config=facmaddpg --env-config=citylearn with t_max=34400 seed=1 evaluate=True test_nepisode=10 runner=episode batch_size_run=1 batch_size=1 checkpoint_path=$currdir/results/models/facmaddpg__2021-07-10_02-52-01/ save_replay=True > logs/facmaddpg-test.log 2>&1 &
-
-#export CUDA_VISIBLE_DEVICES=1 && python src/main.py --config=maddpg    --env-config=citylearn with t_max=210242 seed=1 > logs/1-maddpg.log    2>&1 &
-#export CUDA_VISIBLE_DEVICES=2 && python src/main.py --config=iql-cem   --env-config=citylearn with t_max=210242 seed=1 > logs/1-iql-cem.log   2>&1 &
-#export CUDA_VISIBLE_DEVICES=3 && python src/main.py --config=iql-naf   --env-config=citylearn with t_max=1051210 seed=1 > logs/1-iql-naf.log   2>&1 &
-#export CUDA_VISIBLE_DEVICES=1 && python src/main.py --config=comix     --env-config=citylearn with t_max=100000 seed=1 > logs/1-comix.log     2>&1 &
-#export CUDA_VISIBLE_DEVICES=2 && python src/main.py --config=comix-naf --env-config=citylearn with t_max=100000 seed=1 > logs/1-comix-naf.log 2>&1 &
-#export CUDA_VISIBLE_DEVICES=3 && python src/main.py --config=covdn     --env-config=citylearn with t_max=100000 seed=1 > logs/1-covdn.log     2>&1 &
-#export CUDA_VISIBLE_DEVICES=7 && python src/main.py --config=covdn-naf --env-config=citylearn with t_max=100000 seed=1 > logs/1-covdn-naf.log 2>&1 &
+for i in "${!names[@]}"
+do
+    export CUDA_VISIBLE_DEVICES=${gpus[i]} && python src/main.py --config=${names[i]} --env-config=citylearn with t_max=262800 seed=1 evaluate=True test_nepisode=10 runner=episode batch_size_run=1 batch_size=1 checkpoint_path=$currdir/results/models/${names[i]}__1/ save_replay=True > logs/${names[i]}__1-test.log 2>&1 &
+done
