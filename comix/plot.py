de652fd2e0202385139a5db86d14e49e8815051c
Bo Liu <bliu0201@gmail.com>
2021-07-19 15:48:02 -0500

    update

--- comix/plot.py
+++ comix/plot.py
@@ -1,74 +1,116 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 import json
 
-methods = ["comix-naf","covdn","facmaddpg"]
+methods = ["comix", "comix-naf", "covdn", "covdn-naf", "iql-cem", "facmaddpg", "marlisa"]
+color_maps = {}
+for i, m in enumerate(methods):
+    color_maps[m] = f"C{i}"
+
 sacreds = [1, 2, 3]
 
+seasons = ["spring", "summer", "fall", "winter"]
+
 season_intervals = {
-    "spring": (0, 24*30*3),
-    "summer": (24*30*3, 24*30*6),
-    "fall":   (24*30*6, 24*30*9),
-    "winter": (24*30*9, 24*30*12),
+    "spring": (24*(31+28), 24*(31+28+31+30+31)),
+    "summer": (24*(31+28+31+30+31), 24*(31+28+31+30+31+30+31+31)),
+    "fall":   (24*(31+28+31+30+31+30+31+31), 24*(31+28+31+30+31+30+31+31+30+31+30)),
+    "winter": (24*(-31), 24*(31+28)),
 }
 
 results = {
     "net_electric_consumption_no_pv_no_storage": {},
     "net_electric_consumption_no_storage": {},
     "net_electric_consumption": {},
+    "cost": {},
 }
 
+keys = None
 for method in methods:
-    res = np.load(f"./saved_envs/{method}")
+    res = np.load(f"./saved_envs/{method}", allow_pickle=True)
     ne1 = res["ne1"]
     ne2 = res["ne2"]
     ne3 = res["ne3"]
+    try:
+        cost = res["cost"].item()
+    except:
+        cost = res["cost"][1]
+        tmp = {}
+        for key in cost:
+            tmp[key[:-8]] = cost[key]
+        cost = tmp
     results["net_electric_consumption_no_pv_no_storage"][method] = ne1
     results["net_electric_consumption_no_storage"][method] = ne2
     results["net_electric_consumption"][method] = ne3
+    results["cost"][method] = cost
+    keys = cost.keys()
 
+fs = 20
+lw = 5
 
-seasons = ["spring", "summer", "fall", "winter"]
-color_maps = {
-    "comix-naf": "C0",
-    "covdn": "C1",
-    "facmaddpg": "C2",}
+### plot the season electric consumption
 
-fs = 10
 for season in seasons:
-    fig, axs = plt.subplots(1,3, figsize=(20, 5))
+    fig, ax = plt.subplots(1,1,figsize=(5, 5))
     year = 4
     rg = season_intervals[season]
     interval = range((year-1)*8760 + rg[0], (year-1)*8760 + rg[1])
+
     x = np.arange(24)
-    for method in methods:
-        y = results["net_electric_consumption_no_pv_no_storage"][method][interval].reshape(-1, 24).mean(0)
-        axs[0].plot(x, y, label=method, color=color_maps[method], linewidth=2)
-    axs[0].legend(fontsize=fs)
-    axs[0].set_ylabel("net electricity consumption", fontsize=fs)
-    axs[0].set_xlabel("hour", fontsize=fs)
-    axs[0].set_ylim(0, 450)
-    axs[0].grid()
-    axs[0].set_title("net_electric_consumption_no_pv_no_storage", fontsize=fs)
-    for method in methods:
-        y = results["net_electric_consumption_no_storage"][method][interval].reshape(-1, 24).mean(0)
-        axs[1].plot(x, y, label=method, color=color_maps[method], linewidth=2)
-    axs[1].legend(fontsize=fs)
-    axs[1].set_xlabel("hour", fontsize=fs)
-    axs[1].set_ylim(0, 450)
-    axs[1].grid()
-    axs[1].set_title(f"{season}\nnet_electric_consumption_no_storage", fontsize=fs)
+    y1 = results["net_electric_consumption_no_pv_no_storage"]["comix"][interval].reshape(-1,24).mean(0)
+    y2 = results["net_electric_consumption_no_storage"]["comix"][interval].reshape(-1,24).mean(0)
+
+    ax.plot(x, y2, label="no_storage", color="tab:gray", linewidth=lw)
+
     for method in methods:
         y = results["net_electric_consumption"][method][interval].reshape(-1, 24).mean(0)
-        axs[2].plot(x, y, label=method, color=color_maps[method], linewidth=2)
-    axs[2].legend(fontsize=fs)
-    axs[2].set_xlabel("hour", fontsize=fs)
-    axs[2].set_ylim(0, 450)
-    axs[2].grid()
-    axs[2].set_title("net_electric_consumption", fontsize=fs)
+        ax.plot(x, y, label=method, color=color_maps[method], linewidth=lw)
+ 
+    #ax.legend(fontsize=fs)
+    ax.set_xlabel("hour", fontsize=fs)
+    ax.set_ylim(0, 300)
+    ax.grid()
+    ax.set_title(season.title(), fontsize=(fs+5))
+    ax.set_ylabel("net electric consumption", fontsize=fs)
+    plt.tight_layout()
     plt.savefig(f"imgs/{season}.png", dpi=400)
     plt.close()
 
+# Create a color palette
+plt.figure()
+colors = [color_maps[x] for x in methods]
+palette = dict(zip(["no storage"]+methods, ["tab:gray"] + colors))
+# Create legend handles manually
+handles = [matplotlib.patches.Patch(color=palette[x], label=x) for x in palette.keys()]
+# Create legend
+plt.legend(handles=handles)
+# Get current axes object and turn off axis
+plt.gca().set_axis_off()
+plt.tight_layout()
+plt.savefig("imgs/season_legend.png", dpi=400)
+plt.close()
+
+
+### plot the cost
+n = len(keys)
+Y = np.arange(n) * (len(methods)*0.1 * 2)
+
+fig, ax = plt.subplots()
+
+for ki, key in enumerate(keys):
+    for i,method in enumerate(methods):
+        plt.barh(Y[ki]+i*0.1, results["cost"][method][key], color=color_maps[method], height=0.1)
+
+
+ax.set_yticks(Y+len(methods)/2*0.1)
+ax.set_yticklabels(keys, fontsize=8)
+ax.axvline(x=1, ymin=0, ymax=1000, linestyle='--', color='gray')
+plt.tight_layout()
+plt.savefig("imgs/cost.png", dpi=400)
+plt.close()
+
+"""
 test_costs = {}
 for method, sacred in zip(methods, sacreds):
     path= f"./results/sacred/{sacred}/info.json"                                        
@@ -85,3 +127,4 @@ plt.legend(fontsize=fs)
 plt.xlabel("training step", fontsize=fs)
 plt.ylabel("average test cost", fontsize=fs)
 plt.savefig("imgs/test_costs.png", dpi=400)
+"""
