4ed4187b1fb16b28a72c1c9c235e949982d48e2d
Kingsley Nweye <etonwana@yahoo.com>
2021-11-16 13:48:00 -0600

    bug fixes in job script

--- reward_function_exploration_tacc_batch_job.sh
+++ reward_function_exploration_tacc_batch_job.sh
@@ -6,6 +6,7 @@
 #SBATCH -t 2:00:00
 #SBATCH --mail-user=nweye@utexas.edu
 #SBATCH --mail-type=all
+#SBATCH -o slurm.out
 #SBATCH -A DemandAnalysis
 
 # moad modules
@@ -16,4 +17,4 @@ export LAUNCHER_WORKDIR=/work/07083/ken658/projects/citylearn/CityLearn
 export LAUNCHER_JOB_FILE=reward_function_exploration.sh
 
 # activate virtual environment
-source /work/07083/ken658/projects/citylearn/env/bn/activate
+source /work/07083/ken658/projects/citylearn/env/bin/activate
