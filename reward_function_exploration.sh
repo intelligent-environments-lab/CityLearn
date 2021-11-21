5cb1de2e1afaf97fcdced0ea563f7547b2a67183
Kingsley Nweye <etonwana@yahoo.com>
2021-11-20 20:19:36 -0600

    added runs for CZ 1 -4

--- reward_function_exploration.sh
+++ reward_function_exploration.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 python -m reward_function_exploration 1 marlisa -spe 87599 -dps 26281
+python -m reward_function_exploration 2 marlisa -spe 87599 -dps 26281
+python -m reward_function_exploration 3 marlisa -spe 87599 -dps 26281
+python -m reward_function_exploration 4 marlisa -spe 87599 -dps 26281
