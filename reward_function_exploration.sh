a1e0d967a01747eb234cf6c0f7f54e16c5887cda
Kingsley Nweye <etonwana@yahoo.com>
2021-11-20 20:05:30 -0600

    updated commands

--- reward_function_exploration.sh
+++ reward_function_exploration.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-python -m reward_function_exploration mixed -sf data/reward_function_exploration/mixed.pkl -lf data/reward_function_exploration/mixed.log
-python -m reward_function_exploration marlisa -sf data/reward_function_exploration/marlisa.pkl -lf data/reward_function_exploration/marlisa.log
-python -m reward_function_exploration ramping_square -sf data/reward_function_exploration/ramping_square.pkl -lf data/reward_function_exploration/ramping_square.log
-python -m reward_function_exploration exponential -sf data/reward_function_exploration/exponential.pkl -lf data/reward_function_exploration/exponential.log
-python -m reward_function_exploration default -sf data/reward_function_exploration/default.pkl -lf data/reward_function_exploration/default.log
+python -m reward_function_exploration 1 marlisa -sf data_reward_function_exploration/reward_function_exploration/marlisa.pkl -lf data_reward_function_exploration/reward_function_exploration/marlisa.log -spe 87599
