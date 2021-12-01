b88aa42704da21b20658b665ae21ead91db816e2
Kingsley Nweye <etonwana@yahoo.com>
2021-12-01 10:42:40 -0600

    removed interpreter line

--- reward_function_exploration.sh
+++ reward_function_exploration.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 python -m reward_function_exploration 1 marlisa -spe 87599 -dps 26281
 python -m reward_function_exploration 2 marlisa -spe 87599 -dps 26281
 python -m reward_function_exploration 3 marlisa -spe 87599 -dps 26281
