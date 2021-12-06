80f904d059ba5656c90bee89be1955a0346cc5e5
Kingsley Nweye <etonwana@yahoo.com>
2021-12-06 14:03:18 -0600

    refactoring

--- reward_function_exploration.py
+++ reward_function_exploration.py
@@ -12,26 +12,50 @@ import cost_function
 from reward_function import reward_function_ma
 
 def run(**kwargs):
-    climate_zone = kwargs['climate_zone']
+    agent_name = kwargs['agent_name']
     reward_style = kwargs['reward_style']
+    simulation_id = f'{agent_name}-{reward_style}'
+    climate_zone_directory = os.path.join('data_reward_function_exploration',f'Climate_Zone_{kwargs["climate_zone"]}')
+    output_directory = os.path.join(climate_zone_directory,'reward_function_exploration')
+    os.makedirs(output_directory,exist_ok=True)
+    
+    # get logger
+    log_filepath = os.path.join(output_directory,f'{simulation_id}.log')
+    logger = get_logger(log_filepath)
+    
+    # run simulation
+    env, agent, step_kwargs, runner = get_run_params(**kwargs)
+    kwargs = {**kwargs,'env':env,'agent':agent,'step_kwargs':step_kwargs,'logger':logger}
+    start = time.time()
+    env, agent = runner(**kwargs)
+    logger.debug(f'Loss - {env.cost()}, Simulation time (min) - {(time.time()-start)/60.0}')
+    data = {'env':env,'agents':agent}
+    simulation_filepath = os.path.join(output_directory,f'{simulation_id}.pkl')
+    save(data,filepath=simulation_filepath)
+
+def get_run_params(**kwargs):
+    env = get_env(**kwargs)
+    step_kwargs = {
+        'style':kwargs['reward_style'],
+        'previous_electricity_demand':None,
+        'previous_carbon_intensity':None,
+        'exponential_scaling_factor':0.002,
+    }
+    agent_params = get_agent_params(kwargs['agent_name'],env)
+    agent = agent_params['constructor'](**agent_params['constructor_kwargs'])
+    runner = agent_params['runner']
+    return env, agent, step_kwargs, runner
+
+def get_env(**kwargs):
+    climate_zone = kwargs['climate_zone']
     simulation_period_start = kwargs.get('simulation_period_start',0)
     simulation_period_end = kwargs.get('simulation_period_end',8759)
-    episode_count = kwargs.get('episode_count',1)
-    deterministic_period_start = kwargs.get('deterministic_period_start',3*8760 + 1)
     data_directory = 'data_reward_function_exploration'
     climate_zone_directory = os.path.join(data_directory,f'Climate_Zone_{climate_zone}')
     output_directory = os.path.join(climate_zone_directory,'reward_function_exploration')
     os.makedirs(output_directory,exist_ok=True)
-    
-    # set logger
-    log_filepath = os.path.join(output_directory,f'{reward_style}.log')
-    logging.basicConfig(filename=log_filepath,format='%(asctime)s %(message)s',filemode='w') 
-    logger = logging.getLogger() 
-    logger.setLevel(logging.DEBUG)
-
-    # Load environment
     building_ids = ["Building_"+str(i) for i in range(1,10)]
-    params_env = {
+    env_kwargs = {
         'data_path':Path(climate_zone_directory), 
         'building_attributes':'building_attributes.json', 
         'weather_file':'weather_data.csv', 
@@ -51,98 +75,113 @@ def run(**kwargs):
         'central_agent': False,
         'save_memory': False
     }
-    # Contain the lower and upper bounds of the states and actions, to be provided to the agent to normalize the variables between 0 and 1.
-    # Can be obtained using observations_spaces[i].low or .high
-    env = CityLearn(**params_env)
+    env = CityLearn(**env_kwargs)
+    return env
+
+def get_agent_params(agent_name,env):
     observations_spaces, actions_spaces = env.get_state_action_spaces()
-
-    # Provides information on Building type, Climate Zone, Annual DHW demand, Annual Cooling Demand, Annual Electricity Demand, Solar Capacity, and correllations among buildings
     building_info = env.get_building_information()
-    params_agent = {
-        'building_ids':building_ids,
-        'buildings_states_actions':'buildings_state_action_space.json', 
-        'building_info':building_info,
-        'observation_spaces':observations_spaces, 
-        'action_spaces':actions_spaces, 
-        'hidden_dim':[256,256], 
-        'discount':0.99, 
-        'tau':5e-3, 
-        'lr':3e-4, 
-        'batch_size':256, 
-        'replay_buffer_capacity':1e5, 
-        'regression_buffer_capacity':3e4, 
-        'start_training':600, # Start updating actor-critic networks
-        'exploration_period':7500, # Just taking random actions
-        'start_regression':500, # Start training the regression model
-        'information_sharing':True, # If True -> set the appropriate 'reward_function_ma' in reward_function.py
-        'pca_compression':.95, 
-        'action_scaling_coef':0.5, # Actions are multiplied by this factor to prevent too aggressive actions
-        'reward_scaling':5., # Rewards are normalized and multiplied by this factor
-        'update_per_step':2, # How many times the actor-critic networks are updated every hourly time-step
-        'iterations_as':10,# Iterations of the iterative action selection (see MARLISA paper for more info)
-        'safe_exploration':True
-    } 
-
-    # Instantiating the control agent(s)
-    agents = MARLISA(**params_agent)
-    start = time.time()
-    step_kwargs = {
-        'style':reward_style,
-        'previous_electricity_demand':None,
-        'previous_carbon_intensity':None,
-        'exponential_scaling_factor':0.002,
+    building_ids = env.building_ids
+    agent_params = {
+        'marlisa': {
+            'constructor_kwargs':{
+                'building_ids':building_ids,
+                'buildings_states_actions':'buildings_state_action_space.json', 
+                'building_info':building_info,
+                'observation_spaces':observations_spaces, 
+                'action_spaces':actions_spaces, 
+                'hidden_dim':[256,256], 
+                'discount':0.99, 
+                'tau':5e-3, 
+                'lr':3e-4, 
+                'batch_size':256, 
+                'replay_buffer_capacity':1e5, 
+                'regression_buffer_capacity':3e4, 
+                'start_training':600, # Start updating actor-critic networks
+                'exploration_period':7500, # Just taking random actions
+                'start_regression':500, # Start training the regression model
+                'information_sharing':True, # If True -> set the appropriate 'reward_function_ma' in reward_function.py
+                'pca_compression':.95, 
+                'action_scaling_coef':0.5, # Actions are multiplied by this factor to prevent too aggressive actions
+                'reward_scaling':5., # Rewards are normalized and multiplied by this factor
+                'update_per_step':2, # How many times the actor-critic networks are updated every hourly time-step
+                'iterations_as':10,# Iterations of the iterative action selection (see MARLISA paper for more info)
+                'safe_exploration':True
+            },
+            'constructor':MARLISA,
+            'runner':__run_marlisa
+        },
+        'rbc':{
+            'constructor_kwargs':{
+                'actions_spaces':actions_spaces
+            },
+            'constructor':RBC_Agent,
+            'runner':__run_rbc
+        },
     }
-    baseline = {'params_env':params_env}
-    timestep_costs = []
+    return agent_params[agent_name]
+
+def __run_marlisa(**kwargs):
+    env = kwargs['env']
+    agent = kwargs['agent']
+    logger = kwargs['logger']
+    step_kwargs = kwargs['step_kwargs']
+    episode_count = kwargs.get('episode_count',1)
+    deterministic_period_start = kwargs.get('deterministic_period_start',3*8760 + 1)
 
     for _ in range(episode_count): 
         state = env.reset()
         done = False
         j = 0
         is_evaluating = False
-        action, coordination_vars = agents.select_action(state, deterministic=is_evaluating)    
+        action, coordination_vars = agent.select_action(state, deterministic=is_evaluating)    
         
         while not done:
-            logger.debug(f'Timestep: {j+1}/{int(params_env["simulation_period"][1])}')
-            next_state, reward, done, misc = env.step(action,**step_kwargs)
-            logger.debug(f'next_state: {next_state}')
-            logger.debug(f'reward: {reward}')
+            logger.debug(f'Timestep: {j+1}/{int(env.simulation_period[1])}')
+            next_state, reward, done, _ = env.step(action,**step_kwargs)
             step_kwargs['previous_electricity_demand'] = env.buildings_net_electricity_demand
             step_kwargs['previous_carbon_intensity'] = env.current_carbon_intensity
-            action_next, coordination_vars_next = agents.select_action(next_state, deterministic=is_evaluating)
+            logger.debug(f'reward: {reward}')
+            action_next, coordination_vars_next = agent.select_action(next_state, deterministic=is_evaluating)
             logger.debug(f'action_next: {action_next}')
-            agents.add_to_buffer(state, action, reward, next_state, done, coordination_vars, coordination_vars_next)
+            agent.add_to_buffer(state, action, reward, next_state, done, coordination_vars, coordination_vars_next)
             coordination_vars = coordination_vars_next
             state = next_state
             action = action_next
             is_evaluating = (j >= deterministic_period_start)
-            
-            # calculate timestep cost
-            baseline = {**run_baseline(**baseline),**baseline}
-            timestep_cost = {}
-
-            for agent_name, agent_env in zip(['agent','baseline'],[env,baseline['env']]):
-                cost_kwargs = {
-                    'net_electric_consumption':agent_env.net_electric_consumption,
-                    'carbon_emissions':agent_env.carbon_emissions,
-                }
-                timestep_cost[agent_name] = {key:get_cost(key,**cost_kwargs) for key in params_env['cost_function']}
-
-            timestep_costs.append(timestep_cost)
-            logger.debug(f'cost: {timestep_cost}')
-
             j += 1
-            
-    logger.debug(f'Reward style: {reward_style}, Loss - {env.cost()}, Simulation time (min) - {(time.time()-start)/60.0}')
-    data = {
-        'env':env,
-        'agents':agents,
-        'misc':{
-            'timestep_costs':timestep_costs
-        }
-    }
-    simulation_filepath = os.path.join(output_directory,f'{reward_style}.pkl')
-    save(data,filepath=simulation_filepath)
+
+    return env, agent
+
+def __run_rbc(**kwargs):
+    env = kwargs['env']
+    agent = kwargs['agent']
+    logger = kwargs['logger']
+    step_kwargs = kwargs['step_kwargs']
+    episode_count = kwargs.get('episode_count',1)
+
+    for _ in range(episode_count): 
+        _ = env.reset()
+        done = False
+        j = 0
+        
+        while not done:
+            logger.debug(f'Timestep: {j+1}/{int(env.simulation_period[1])}')
+            hour = list(env.buildings.values())[0].sim_results['hour'][env.time_step]
+            action = agent.select_action([hour])
+            _, reward, done, _ = env.step(action,**step_kwargs)
+            step_kwargs['previous_electricity_demand'] = env.buildings_net_electricity_demand
+            step_kwargs['previous_carbon_intensity'] = env.current_carbon_intensity
+            logger.debug(f'reward: {reward}')
+            j += 1
+
+    return env, agent
+
+def get_logger(filepath):
+    logging.basicConfig(filename=filepath,format='%(asctime)s %(message)s',filemode='w') 
+    logger = logging.getLogger() 
+    logger.setLevel(logging.DEBUG)
+    return logger
 
 def get_cost(key,**kwargs):
     reference = {
@@ -159,29 +198,6 @@ def get_cost(key,**kwargs):
     function_kwargs = {k:kwargs.get(k) for k in arg_spec.args if k in kwargs.keys()}
     cost = function(**function_kwargs)
     return cost
-
-def run_baseline(env=None,agent=None,done=None,params_env=None,iterations=1):
-    if env is None:
-        env = CityLearn(**params_env)
-        _, actions_spaces = env.get_state_action_spaces()
-
-        #Instantiatiing the control agent(s)
-        agent = RBC_Agent(actions_spaces)
-        _ = env.reset()
-        done = False
-
-    else:
-        pass
-
-    if not done:
-        for _ in range(iterations):
-            action = agent.select_action([list(env.buildings.values())[0].sim_results['hour'][env.time_step]])
-            _, _, done, _ = env.step(action)
-
-    else:
-        pass
-    
-    return {'env':env,'agent':agent,'done':done}
 
 def save(data,filepath='citylearn.pkl'):
     directory = '/'.join(filepath.split('/')[0:-1])
@@ -208,6 +224,7 @@ def get_climate_zones():
 def main():
     parser = argparse.ArgumentParser(prog='reward_function_exploration',description='Explore different reward functions in CityLearn environment.')
     parser.add_argument('climate_zone',type=str,choices=get_climate_zones(),help='Simulation climate zone.')
+    parser.add_argument('agent_name',type=str,choices=['marlisa','rbc'],help='Simulation agent.')
     parser.add_argument('reward_style',type=str,choices=reward_function_ma.get_styles(),help='Reward function style.')
     parser.add_argument('-e','--episode_count',type=int,default=1,dest='episode_count',help='Number of episodes.')
     parser.add_argument('-sps','--simulation_period_start',type=int,default=0,dest='simulation_period_start',help='Simulation start index.')
