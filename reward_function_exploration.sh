3f3c4f3b582758fbd9c67fb87ce92e366f53f08d
Kingsley Nweye <etonwana@yahoo.com>
2021-12-07 09:50:13 -0600

    renamed

--- reward_function_exploration.sh
+++
@@ -1,3 +0,0 @@
-python -m reward_function_exploration 2 marlisa -spe 87599 -dps 26281
-python -m reward_function_exploration 3 marlisa -spe 87599 -dps 26281
-python -m reward_function_exploration 4 marlisa -spe 87599 -dps 26281
