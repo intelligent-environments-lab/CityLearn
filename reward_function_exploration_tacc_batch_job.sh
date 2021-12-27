f139a50a152764dcb251cc9ee612fe5572c6551f
Kingsley Nweye <etonwana@yahoo.com>
2021-12-27 15:51:27 -0600

    added user env vars

--- reward_function_exploration_tacc_batch_job.sh
+++ reward_function_exploration_tacc_batch_job.sh
@@ -15,8 +15,23 @@ module load launcher
 # activate virtual environment
 source /work/07083/ken658/projects/citylearn/env/bin/activate
 
-# set environment variables
-export LAUNCHER_WORKDIR=/work/07083/ken658/projects/citylearn/CityLearn
-export LAUNCHER_JOB_FILE=reward_function_exploration
+# set user environment variables
+export CLRFE_DATA_PATH="data_reward_function_exploration/Climate_Zone_"
+export CLRFE_STATE_ACTION_SPACE_FILENAME="buildings_state_action_space.json"
+export CLRFE_EPIOSIDE_TIMESTEPS="8760"
+export CLRFE_EPISODES="10"
+export CLRFE_SIMULATION_PERIOD_START="0"
+export CLRFE_SIMULATION_PERIOD_END=$(($CLRFE_EPIOSIDE_TIMESTEPS - 1))
+export CLRFE_DETERMINISTIC_PERIOD_START=$(($CLRFE_EPIOSIDE_TIMESTEPS * 7))
+export CLRFE_REGRESSION_BUFFER_CAPACITY="30000"
+export CLRFE_START_TRAINING=$(($CLRFE_EPIOSIDE_TIMESTEPS * 3))
+export CLRFE_EXPLORATION_PERIOD=$(($CLRFE_START_TRAINING + 1))
+export CLRFE_START_REGRESSION=$(($CLRFE_EPIOSIDE_TIMESTEPS * 1))
+export CLRFE_ACTION_SCALING_COEF="0.5"
+export CLRFE_REWARD_SCALING="5.0"
+
+# set launcher environment variables
+export LAUNCHER_WORKDIR="/work/07083/ken658/projects/citylearn/CityLearn"
+export LAUNCHER_JOB_FILE="reward_function_exploration"
 
 ${LAUNCHER_DIR}/paramrun
