7e317c0b66a26757953f034a661d865d08d4a358
Kingsley Nweye <etonwana@yahoo.com>
2022-02-02 18:07:15 -0600

    added basic and optimized rbc

--- agents/rbc.py
+++ agents/rbc.py
@@ -7,26 +7,48 @@ class RBC:
         
     def reset_action_tracker(self):
         self.action_tracker = []
-        
-    def select_action(self, states):
-        hour_day = states[0][0]
-        multiplier = 0.4
-        # Daytime: release stored energy  2*0.08 + 0.1*7 + 0.09
-        a = [[0.0 for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
-        if hour_day >= 7 and hour_day <= 11:
-            a = [[-0.05 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
-        elif hour_day >= 12 and hour_day <= 15:
-            a = [[-0.05 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
-        elif hour_day >= 16 and hour_day <= 18:
-            a = [[-0.11 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
-        elif hour_day >= 19 and hour_day <= 22:
-            a = [[-0.06 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
-        
+
+    def select_action(self):
+        raise NotImplementedError
+
+class BasicRBC(RBC):
+    def __init__(self, actions_spaces):
+        super().__init__(actions_spaces)
+
+    def select_action(self,hour):
+        # Daytime: release stored energy
+        if hour >= 9 and hour <= 21:
+            a = [[-0.08 for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
         # Early nightime: store DHW and/or cooling energy
-        if hour_day >= 23 and hour_day <= 24:
+        elif (hour >= 1 and hour <= 8) or (hour >= 22 and hour <= 24):
+            a = [[0.091 for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
+        else:
+            a = [[0.0 for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
+            
+        self.action_tracker.append(a)
+        return np.array(a, dtype = 'object')
+
+class OptimizedRBC(RBC):
+    def __init__(self, actions_spaces):
+        super().__init__(actions_spaces)
+        
+    def select_action(self,hour,multiplier=0.4):
+        # Daytime: release stored energy  2*0.08 + 0.1*7 + 0.09
+        if hour >= 7 and hour <= 11:
+            a = [[-0.05 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
+        elif hour >= 12 and hour <= 15:
+            a = [[-0.05 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
+        elif hour >= 16 and hour <= 18:
+            a = [[-0.11 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
+        elif hour >= 19 and hour <= 22:
+            a = [[-0.06 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
+        # Early nightime: store DHW and/or cooling energy
+        elif hour >= 23 and hour <= 24:
             a = [[0.085 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
-        elif hour_day >= 1 and hour_day <= 6:
+        elif hour >= 1 and hour <= 6:
             a = [[0.1383 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
+        else:
+            a = [[0.0 for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
 
         self.action_tracker.append(a)
         return np.array(a, dtype = 'object')
