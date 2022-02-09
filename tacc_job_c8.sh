4758bd09fa7289a1ffc095c8c0bbd037e8e0e191
Kingsley Nweye <etonwana@yahoo.com>
2022-02-09 09:14:53 -0600

    added tacc job for c8

---
+++ tacc_job_c8.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -p normal
+#SBATCH -J citylearn_grid_search
+#SBATCH -N 50
+#SBATCH --tasks-per-node 1
+#SBATCH -t 48:00:00
+#SBATCH --mail-user=nweye@utexas.edu
+#SBATCH --mail-type=all
+#SBATCH -o slurm_c8.out
+#SBATCH -A DemandAnalysis
+
+# load modules
+module load launcher
+
+# activate virtual environment
+source /work/07083/ken658/projects/citylearn/env/bin/activate
+
+# set launcher environment variables
+export LAUNCHER_WORKDIR="/work/07083/ken658/projects/citylearn/CityLearn"
+export LAUNCHER_JOB_FILE="tacc_launcher_job_c8"
+
+${LAUNCHER_DIR}/paramrun
