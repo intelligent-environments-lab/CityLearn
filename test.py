a4af003e398177359ea3bb585c95a1583407e54d
Kingsley Nweye <etonwana@yahoo.com>
2022-06-15 11:50:43 -0500

    updated test script

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 import pickle
+from citylearn.citylearn import CityLearnEnv
 from citylearn.simulator import Simulator
 from citylearn.utilities import read_json
 
 RESULT_FILEPATH = 'simulation.pkl'
-SCHEMA_FILEPATH = 'data/cc2022_d1/schema.json'
+SCHEMA_FILEPATH = '/Users/kingsleyenweye/Desktop/INTELLIGENT_ENVIRONMENT_LAB/citylearn/CityLearn/data/cc2022_d1/schema.json'
 
 def main():
     schema = read_json(SCHEMA_FILEPATH)
-    citylearn_env, agents, episodes = Simulator.load(schema)
-    simulator = Simulator(citylearn_env, agents, episodes)
+    citylearn_env = CityLearnEnv(schema)
+    agents = citylearn_env.load_agents()
+    simulator = Simulator(citylearn_env, agents, schema['episodes'])
     simulator.simulate()
 
     with open(RESULT_FILEPATH,'wb') as f:
