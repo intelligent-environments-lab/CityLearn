2b3b936c6d097400b4987824129766a177bf73f7
Dipam Chakraborty <dipamc77@gmail.com>
2022-06-30 20:38:43 +0530

    change root directory loading for filepath schema

--- playground_env.py
+++ playground_env.py
@@ -1,17 +1,18 @@
 from citylearn.citylearn import CityLearnEnv
 
 if __name__ == '__main__':
-    schema = 'citylearn_challenge_2022_phase_1'
+    schema = './citylearn/data/citylearn_challenge_2022_phase_1/schema.json'
     env = CityLearnEnv(schema=schema)
     print("Citylearn environment created")
-    all_obs = env.reset()
-    num_buildings = len(env.buildings)
-    done = False
-    tsteps = 0
-    while not done:
-        actions = [action_space.sample() for action_space in env.action_space]
-        all_obs, all_rew, done, all_info = env.step(actions)
-        tsteps += 1
-        if tsteps % 50 == 0:
-            print(f"Time steps: {tsteps}")
-    print(f"Episode completed in {tsteps} time steps")
+    for _ in range(2):
+        all_obs = env.reset()
+        num_buildings = len(env.buildings)
+        done = False
+        tsteps = 0
+        while not done:
+            actions = [action_space.sample() for action_space in env.action_space]
+            all_obs, all_rew, done, all_info = env.step(actions)
+            tsteps += 1
+            if tsteps % 50 == 0:
+                print(f"Time steps: {tsteps}")
+        print(f"Episode completed in {tsteps} time steps")
