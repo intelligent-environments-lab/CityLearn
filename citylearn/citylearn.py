4f1bd8d0986b641145ca88a1ccdd9fe09eab657b
Kingsley Nweye <etonwana@yahoo.com>
2022-07-18 07:31:05 -0500

    removed unused code

--- citylearn/citylearn.py
+++ citylearn/citylearn.py
@@ -546,39 +546,6 @@ class CityLearnEnv(Environment, Env):
     def render(self):
         """Only applies to the CityLearn Challenge 2022 setup."""
 
-        data = {
-            'reward':{
-                'cumulative':pd.DataFrame(self.rewards).sum(axis=1).rolling(window=len(self.rewards),min_periods=1).sum(), # rewards rolling sum
-                'average':pd.DataFrame(self.rewards).mean(axis=1).to_numpy(), # average reward per time step
-            },
-            'cost':{
-                'net_electricity_consumption':CostFunction.net_electricity_consumption(self.net_electricity_consumption),
-                'carbon_emissions':CostFunction.carbon_emissions(self.net_electricity_consumption_emission),
-                'price':CostFunction.price(self.net_electricity_consumption_price),
-            },
-            'building':[]
-        }
-
-        for b in self.buildings:
-            data['building'].append({
-                'name':b.name, # building name
-                'connection':{
-                    'electrical_storage':b.electrical_storage_electricity_consumption[b.time_step - 1], # if (-) flow left, else right
-                    'pv':abs(b.solar_generation[b.time_step - 1]), # unidirectional
-                    'grid':b.net_electricity_consumption[b.time_step - 1] # if (-) flow down, else up
-                },
-                'graph':{
-                    'bar':{
-                        'electrical_storage_soc':b.electrical_storage.soc[b.time_step - 1]/b.electrical_storage.capacity_history[-2]
-                    },
-                    'line':{
-                        'net_electricity_consumption':b.net_electricity_consumption[-24:],
-                        'net_electricity_consumption_without_storage':b.net_electricity_consumption_without_storage[-24:],
-                        'net_electricity_consumption_without_storage_and_pv':b.net_electricity_consumption_without_storage_and_pv[-24:],
-                    },
-                }
-            })
-
         canvas, canvas_size, draw_obj, color = get_background()
         num_buildings = len(self.buildings)
 
