6b941397be846a7fb6ca28d66a351e8c5d79e846
Jiahan Xie <765130715@qq.com>
2022-07-27 10:15:41 -0400

    add custominzable user config

---
+++ maac/user_config.py
@@ -0,0 +1,21 @@
+import os.path as osp
+
+# Default neural network backend for each algo
+# (Must be either 'tf1' or 'pytorch')
+DEFAULT_BACKEND = {
+    'maac': 'pytorch'
+}
+
+# Where experiment outputs are saved by default:
+DEFAULT_DATA_DIR = osp.join(osp.abspath(osp.dirname(osp.dirname(__file__))), 'data')
+
+# Whether to automatically insert a date and time stamp into the names of
+# save directories:
+FORCE_DATESTAMP = False
+
+# Whether GridSearch provides automatically-generated default shorthands:
+DEFAULT_SHORTHAND = True
+
+# Tells the GridSearch how many seconds to pause for before launching
+# experiments.
+WAIT_BEFORE_LAUNCH = 5
