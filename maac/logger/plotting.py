c140fa42d1a350168b0ddb663be2e5e8e03a51cc
Jiahan Xie <765130715@qq.com>
2022-07-27 10:11:43 -0400

    add plotting across episodes

--- maac/logger/plotting.py
+++ maac/logger/plotting.py
@@ -4,7 +4,7 @@ import pandas as pd
 from collections import namedtuple
 from matplotlib import pyplot as plt
 
-EpisodeStats = namedtuple("Stats",["episode_lengths", "episode_rewards"])
+EpisodeStats = namedtuple("Stats", ["episode_rewards", "episode_costs"])
 
 
 def plot_cost_to_go_mountain_car(env, estimator, num_tiles=20):
@@ -59,40 +59,36 @@ def plot_value_function(V, title="Value Function"):
     plot_surface(X, Y, Z_ace, "{} (Usable Ace)".format(title))
 
 
-
-def plot_episode_stats(stats, smoothing_window=10, noshow=False):
-    # Plot the episode length over time
-    fig1 = plt.figure(figsize=(10,5))
-    plt.plot(stats.episode_lengths)
-    plt.xlabel("Episode")
-    plt.ylabel("Episode Length")
-    plt.title("Episode Length over Time")
-    if noshow:
-        plt.close(fig1)
-    else:
-        plt.show(fig1)
-
+def plot_episode_stats(stats, smoothing_window=1, noshow=False):
+    """
+    Plot episode stats
+    :param stats:
+    :param smoothing_window:
+    :param noshow:
+    :return:
+    """
     # Plot the episode reward over time
-    fig2 = plt.figure(figsize=(10,5))
+    fig1 = plt.figure(figsize=(10, 5))
     rewards_smoothed = pd.Series(stats.episode_rewards).rolling(smoothing_window, min_periods=smoothing_window).mean()
     plt.plot(rewards_smoothed)
     plt.xlabel("Episode")
     plt.ylabel("Episode Reward (Smoothed)")
     plt.title("Episode Reward over Time (Smoothed over window size {})".format(smoothing_window))
     if noshow:
+        plt.close(fig1)
+    else:
+        plt.show(fig1)
+
+    # Plot the episode costs over time
+    fig2 = plt.figure(figsize=(10, 5))
+    cost_smoothed = pd.Series(stats.episode_costs).rolling(smoothing_window, min_periods=smoothing_window).mean()
+    plt.plot(cost_smoothed)
+    plt.xlabel("Episode")
+    plt.ylabel("Episode Total Cost (Smoothed)")
+    plt.title("Episode Total Cost over Time (Smoothed over window size {})".format(smoothing_window))
+    if noshow:
         plt.close(fig2)
     else:
         plt.show(fig2)
 
-    # Plot time steps and episode number
-    fig3 = plt.figure(figsize=(10,5))
-    plt.plot(np.cumsum(stats.episode_lengths), np.arange(len(stats.episode_lengths)))
-    plt.xlabel("Time Steps")
-    plt.ylabel("Episode")
-    plt.title("Episode per time step")
-    if noshow:
-        plt.close(fig3)
-    else:
-        plt.show(fig3)
-
-    return fig1, fig2, fig3
+    return fig1, fig2
