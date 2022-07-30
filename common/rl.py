3364c62daf5e72bd1b24e4c9cfd47e422fa3a7e4
Jiahan Xie <765130715@qq.com>
2022-07-30 01:08:51 -0400

    pylint reformat

--- common/rl.py
+++ common/rl.py
@@ -5,24 +5,26 @@ from torch.distributions import Normal
 import random
 import numpy as np
 
+
 class PolicyNetwork(nn.Module):
-    def __init__(self, 
-                 num_inputs, 
-                 num_actions, 
-                 action_space, 
-                 action_scaling_coef, 
-                 hidden_dim = [400,300],
-                 init_w = 3e-3, 
-                 log_std_min = -20, 
-                 log_std_max = 2, 
-                 epsilon = 1e-6):
-        
+    def __init__(self,
+                 num_inputs,
+                 num_actions,
+                 action_space,
+                 action_scaling_coef,
+                 hidden_dim=None,
+                 init_w=3e-3,
+                 log_std_min=-20,
+                 log_std_max=2,
+                 epsilon=1e-6):
         super(PolicyNetwork, self).__init__()
 
+        if hidden_dim is None:
+            hidden_dim = [400, 300]
         self.log_std_min = log_std_min
         self.log_std_max = log_std_max
         self.epsilon = epsilon
-        
+
         self.linear1 = nn.Linear(num_inputs, hidden_dim[0])
         self.linear2 = nn.Linear(hidden_dim[0], hidden_dim[1])
 
@@ -66,60 +68,63 @@ class PolicyNetwork(nn.Module):
         self.action_scale = self.action_scale.to(device)
         self.action_bias = self.action_bias.to(device)
         return super(PolicyNetwork, self).to(device)
-    
+
+
 class ReplayBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.buffer = []
         self.position = 0
-    
+
     def push(self, state, action, reward, next_state, done):
         if len(self.buffer) < self.capacity:
             self.buffer.append(None)
-        
+
         self.buffer[self.position] = (state, action, reward, next_state, done)
         self.position = (self.position + 1) % self.capacity
-    
+
     def sample(self, batch_size):
         batch = random.sample(self.buffer, batch_size)
         state, action, reward, next_state, done = map(np.stack, zip(*batch))
         return state, action, reward, next_state, done
-    
+
     def __len__(self):
         return len(self.buffer)
-        
+
+
 class RegressionBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.x = []
         self.y = []
         self.position = 0
-    
+
     def push(self, variables, targets):
-        if len(self.x) < self.capacity and len(self.x)==len(self.y):
+        if len(self.x) < self.capacity and len(self.x) == len(self.y):
             self.x.append(None)
             self.y.append(None)
-        
+
         self.x[self.position] = variables
         self.y[self.position] = targets
         self.position = (self.position + 1) % self.capacity
-    
+
     def __len__(self):
         return len(self.x)
-    
+
+
 class SoftQNetwork(nn.Module):
-    def __init__(self, num_inputs, num_actions, hidden_size=[400,300], init_w=3e-3):
+    def __init__(self, num_inputs, num_actions, hidden_size=[400, 300], init_w=3e-3):
         super(SoftQNetwork, self).__init__()
-        
+
         self.linear1 = nn.Linear(num_inputs + num_actions, hidden_size[0])
         self.linear2 = nn.Linear(hidden_size[0], hidden_size[1])
         self.linear3 = nn.Linear(hidden_size[1], 1)
         self.ln1 = nn.LayerNorm(hidden_size[0])
         self.ln2 = nn.LayerNorm(hidden_size[1])
-        
+
         self.linear3.weight.data.uniform_(-init_w, init_w)
         self.linear3.bias.data.uniform_(-init_w, init_w)
-        
+
     def forward(self, state, action):
         x = torch.cat([state, action], 1)
         x = self.ln1(F.relu(self.linear1(x)))
