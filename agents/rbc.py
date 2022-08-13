6ee929cc42207ff0f19b6bcfa5c72474d5978085
Jiahan Xie <765130715@qq.com>
2022-08-13 12:38:18 -0400

    modify RBC policy

--- agents/rbc.py
+++ agents/rbc.py
@@ -11,29 +11,38 @@ class RBC:
 
     def select_action(self, states):
         hour_day = states[0][0]
-        multiplier = 0.4
-        # Daytime: release stored energy  2*0.08 + 0.1*7 + 0.09
-        a = [[0.0 for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
-        if 7 <= hour_day <= 11:
-            a = [[-0.05 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in
-                 range(len(self.actions_spaces))]
-        elif 12 <= hour_day <= 15:
-            a = [[-0.05 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in
-                 range(len(self.actions_spaces))]
-        elif 16 <= hour_day <= 18:
-            a = [[-0.11 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in
-                 range(len(self.actions_spaces))]
-        elif 19 <= hour_day <= 22:
-            a = [[-0.06 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in
-                 range(len(self.actions_spaces))]
+        # multiplier = 0.4
+        # # Daytime: release stored energy  2*0.08 + 0.1*7 + 0.09
+        # a = [[0.0 for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
+        # if 7 <= hour_day <= 11:
+        #     a = [[-0.05 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in
+        #          range(len(self.actions_spaces))]
+        # elif 12 <= hour_day <= 15:
+        #     a = [[-0.05 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in
+        #          range(len(self.actions_spaces))]
+        # elif 16 <= hour_day <= 18:
+        #     a = [[-0.11 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in
+        #          range(len(self.actions_spaces))]
+        # elif 19 <= hour_day <= 22:
+        #     a = [[-0.06 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in
+        #          range(len(self.actions_spaces))]
 
-        # Early nighttime: store DHW and/or cooling energy
-        if 23 <= hour_day <= 24:
-            a = [[0.085 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in
-                 range(len(self.actions_spaces))]
-        elif 1 <= hour_day <= 6:
-            a = [[0.1383 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in
-                 range(len(self.actions_spaces))]
+        # # Early nighttime: store DHW and/or cooling energy
+        # if 23 <= hour_day <= 24:
+        #     a = [[0.085 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in
+        #          range(len(self.actions_spaces))]
+        # elif 1 <= hour_day <= 6:
+        #     a = [[0.1383 * multiplier for _ in range(len(self.actions_spaces[i].sample()))] for i in
+        #          range(len(self.actions_spaces))]
+
+        # Daytime: release stored energy
+        a = [[0.0 for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
+        if 9 <= hour_day <= 21:
+            a = [[-0.08 for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
+
+        # Early nightime: store DHW and/or cooling energy
+        if (1 <= hour_day <= 8) or (22 <= hour_day <= 24):
+            a = [[0.091 for _ in range(len(self.actions_spaces[i].sample()))] for i in range(len(self.actions_spaces))]
 
         self.action_tracker.append(a)
         return np.array(a, dtype='object')
