b788a9023621d622cf26196a4cb3e28629f2c727
Kingsley Nweye <etonwana@yahoo.com>
2023-01-27 11:28:25 -0600

    updated cli

--- citylearn/__main__.py
+++ citylearn/__main__.py
@@ -1,5 +1,4 @@
 import argparse
-from datetime import datetime
 from citylearn.__init__ import __version__
 import inspect
 from pathlib import Path
@@ -8,20 +7,20 @@ import sys
 from citylearn.citylearn import CityLearnEnv
 from citylearn.simulator import Simulator
 
-def simulate(schema: str, result_filepath: str = None):
-    citylearn_env = CityLearnEnv(schema)
-    agents = citylearn_env.load_agent()
-    simulator = Simulator(citylearn_env, agents, schema['episodes'])
+def simulate(schema: str, result_filepath: str = None, keep_env_history: bool = None):
+    env = CityLearnEnv(schema)
+    agents = env.load_agent()
+    simulator = Simulator(env, agents, episodes=env.schema['episodes'], keep_env_history=keep_env_history)
 
     try:
         simulator.simulate()
     
     finally:
-        result_filepath = f'simulation_{datetime.utcnow().replace(microsecond=0)}' if result_filepath is None else result_filepath
+        result_filepath = f'simulation_output' if result_filepath is None else result_filepath
         result_filepath = '.'.join(result_filepath.split('.')) + '.pkl'
 
         with open(result_filepath, 'wb') as f:
-            pickle.dump(citylearn_env, f)
+            pickle.dump(simulator, f)
 
 def main():
     parser = argparse.ArgumentParser(prog='citylearn', formatter_class=argparse.ArgumentDefaultsHelpFormatter, description=('''
@@ -31,8 +30,9 @@ def main():
     parser.add_argument('--version', action='version', version='%(prog)s' + f' {__version__}')
     subparsers = parser.add_subparsers(title='subcommands', required=True, dest='subcommands')
     subparser_simulate = subparsers.add_parser('simulate', description='Run simulation.')
-    subparser_simulate.add_argument('schema', type=Path, help='CityLearn data set name or schema absolute filepath.')
+    subparser_simulate.add_argument('schema', type=str, help='CityLearn data set name or schema absolute filepath.')
     subparser_simulate.add_argument('-f', '--result_filepath', type=Path, dest='result_filepath', help='Filepath to write simulation pickle object to upon completion.')
+    subparser_simulate.add_argument('-k', '--keep_env_history', action='store_true', dest='keep_env_history', help='ndicator to store environment state at the end of each episode.')
     subparser_simulate.set_defaults(func=simulate)
     args = parser.parse_args()
     arg_spec = inspect.getfullargspec(args.func)
