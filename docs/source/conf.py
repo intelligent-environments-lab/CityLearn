b64fdcd3896613da08e074de2277e8b85b8ad7b3
Kingsley Nweye <etonwana@yahoo.com>
2023-01-27 01:53:05 -0600

    updated html context

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,3 +67,12 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+html_context = {
+  'display_github': True,
+  'github_user': 'intelligent-environments-lab',
+  'github_repo': 'CityLearn',
+  'github_version': 'master/docs/',
+  'last_updated': True,
+}
