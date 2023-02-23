b03f8c4c289778e3192678bf2f55c55573379858
Kingsley Nweye <etonwana@yahoo.com>
2023-02-23 15:45:43 -0600

    refactored

--- tests/stable_baselines.py
+++ tests/stable_baselines.py
@@ -1,13 +1,26 @@
 import sys
 sys.path.insert(0, '..')
+from stable_baselines3 import SAC
 from stable_baselines3.common.env_checker import check_env
-from citylearn.citylearn import CityLearnEnvStableBaselines3Wrapper
+from citylearn.citylearn import CityLearnEnv, StableBaselines3Wrapper
 
+# Initialize environment
 dataset_name = 'citylearn_challenge_2022_phase_1'
-env = CityLearnEnvStableBaselines3Wrapper(dataset_name, central_agent=True)
+env = CityLearnEnv(dataset_name)
 
+# Wrap for SB3 compatibility
+env = StableBaselines3Wrapper(env)
+
+# Perform compatibility check
 try:
     check_env(env)
-    print('Passed test!! CityLearn is compatible with SB3 when using the CityLearnEnvStableBaselines3Wrapper.')
-except Exception as e:
-    print(e)
+    print('Passed test!! CityLearn is compatible with SB3 when using the StableBaselines3Wrapper.')
+finally:
+    pass
+
+# Run simulation with SAC policy
+model = SAC('MlpPolicy', env, verbose=1, seed=0)
+model.learn(total_timesteps=1000, log_interval=4)
+
+# Evaluation
+print(env.evaluate())
