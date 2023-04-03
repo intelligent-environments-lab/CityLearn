e56a054183b49de747fe648674a2630a9ac51017
Kingsley Nweye <etonwana@yahoo.com>
2023-04-03 18:51:03 -0500

    refactored test files

--- tests/stable_baselines.py
+++ tests/stable_baselines.py
@@ -2,11 +2,16 @@ import sys
 sys.path.insert(0, '..')
 from stable_baselines3 import SAC
 from stable_baselines3.common.env_checker import check_env
-from citylearn.citylearn import CityLearnEnv, StableBaselines3Wrapper
+from citylearn.citylearn import CityLearnEnv
+from citylearn.wrappers import NormalizedObservationWrapper, StableBaselines3Wrapper
 
 # Initialize environment
 dataset_name = 'citylearn_challenge_2022_phase_1'
-env = CityLearnEnv(dataset_name)
+env = CityLearnEnv(dataset_name, central_agent=True)
+env.buildings = env.buildings[0:1]
+
+# Normalization wrapper
+env = NormalizedObservationWrapper(env)
 
 # Wrap for SB3 compatibility
 env = StableBaselines3Wrapper(env)
@@ -19,8 +24,8 @@ finally:
     pass
 
 # Run simulation with SAC policy
-model = SAC('MlpPolicy', env, verbose=1, seed=0)
-model.learn(total_timesteps=1000, log_interval=4)
+model = SAC('MlpPolicy', env, verbose=2, learning_starts=8760, seed=0)
+model.learn(total_timesteps=8760*2, log_interval=8760)
 
 # Evaluation
 print(env.evaluate())
--- tests/test_environment.py
+++ tests/test_environment.py
@@ -3,7 +3,6 @@ import time
 import sys
 sys.path.insert(0, '..')
 from citylearn.citylearn import CityLearnEnv
-from citylearn.simulator import Simulator
 
 RESULT_FILEPATH = 'test_environment.pkl'
 schema = 'citylearn_challenge_2020_climate_zone_1'
@@ -11,17 +10,14 @@ schema = 'citylearn_challenge_2020_climate_zone_1'
 def main():
     # simulation
     env = CityLearnEnv(schema)
-    agent = env.load_agent()
-    simulator = Simulator(env, agent, 2, logging_level=5)
-    simulator.simulate()
+    model = env.load_agent()
+    model.learn(episodes=1, deterministic_finish=False)
 
-    print(simulator.env_history)
-    print(simulator.env.evaluate())
-    assert False
+    print(env.evaluate())
 
     # save simulator to file
     with open(RESULT_FILEPATH, 'wb') as f:
-        pickle.dump(simulator,f)
+        pickle.dump(model, f)
 
 if __name__ == '__main__':
     start_time = time.time()
