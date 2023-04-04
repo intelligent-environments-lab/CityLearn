f86b0bd5a659c1b5beed6d5950fc5859fcbf3215
Kingsley Nweye <etonwana@yahoo.com>
2023-04-03 19:08:25 -0500

    updated examples

--- examples/custom_reward_function.py
+++ examples/custom_reward_function.py
@@ -10,16 +10,24 @@
 #********************** END ***********************
 
 from typing import List
+from citylearn.citylearn import CityLearnEnv
 from citylearn.reward_function import RewardFunction
 
 class CustomReward(RewardFunction):
-    def __init__(self, agent_count: int, electricity_consumption: List[float], carbon_emission: List[float], electricity_price: List[float]):
-        super().__init__(agent_count, electricity_consumption=electricity_consumption, carbon_emission=carbon_emission, electricity_price=electricity_price)
+    def __init__(self, env: CityLearnEnv):
+        super().__init__(env)
         
     def calculate(self) -> List[float]:
         """Calculates custom user-defined multi-agent reward.
         
-        Reward is the `carbon_emission` for each building.
+        Reward is the :py:atter:`net_electricity_consumption_emission` for entire district if central agent setup 
+        otherwise it is the :py:atter:`net_electricity_consumption_emission` each building.
         """
 
-        return list(self.carbon_emission)
+        if self.env.central_agent:
+            reward = [self.env.net_electricity_consumption_emission[-1]]
+
+        else:
+            reward = [b.net_electricity_consumption_emission[-1] for b in self.env.buildings]
+
+        return reward
