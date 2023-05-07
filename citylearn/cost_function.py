d9c65deaf02332562b848e9e8e909eccaabc7c0e
Kingsley Nweye <etonwana@yahoo.com>
2023-05-07 11:42:28 -0500

    now calculate proportion for only occupied period

--- citylearn/cost_function.py
+++ citylearn/cost_function.py
@@ -216,12 +216,12 @@ class CostFunction:
         Returns
         -------
         unmet: List[float]
-            Rolling proportion of timesteps where the condition 
+            Rolling proportion of occupied timesteps where the condition 
             (dry_bulb_temperature_set_point - band) <= indoor_dry_bulb_temperature <= (dry_bulb_temperature_set_point + band) is not met.
         too_cold: List[float]
-            Rolling proportion of timesteps where the condition indoor_dry_bulb_temperature < (dry_bulb_temperature_set_point - band) is met.
+            Rolling proportion of occupied timesteps where the condition indoor_dry_bulb_temperature < (dry_bulb_temperature_set_point - band) is met.
         too_hot: List[float]
-            Rolling proportion of timesteps where the condition indoor_dry_bulb_temperature > (dry_bulb_temperature_set_point + band) is met.
+            Rolling proportion of occupied timesteps where the condition indoor_dry_bulb_temperature > (dry_bulb_temperature_set_point + band) is met.
         minimum_delta: List[float]
             Rolling minimum of indoor_dry_bulb_temperature - dry_bulb_temperature_set_point.
         maximum_delta: List[float]
@@ -238,21 +238,22 @@ class CostFunction:
             'dry_bulb_temperature_set_point': dry_bulb_temperature_set_point,
             'occupant_count': [1]*len(indoor_dry_bulb_temperature) if occupant_count is None else occupant_count
         })
+        occupied_time_step_count = data[data['occupant_count'] >= 1.0].shape[0]
         data['delta'] = data['indoor_dry_bulb_temperature'] - data['dry_bulb_temperature_set_point']
         data.loc[data['occupant_count'] < 1.0, 'delta'] = 0.0
         data['unmet'] = 0
         data.loc[data['delta'].abs() > band, 'unmet'] = 1
-        data['unmet'] = data['unmet'].rolling(window=data.shape[0],min_periods=1).sum()/data.shape[0]
+        data['unmet'] = data['unmet'].rolling(window=data.shape[0],min_periods=1).sum()/occupied_time_step_count
 
         # too cold
         data['too_cold'] = 0
         data.loc[data['delta'] < -band, 'too_cold'] = 1
-        data['too_cold'] = data['too_cold'].rolling(window=data.shape[0],min_periods=1).sum()/data.shape[0]
+        data['too_cold'] = data['too_cold'].rolling(window=data.shape[0],min_periods=1).sum()/occupied_time_step_count
 
         # too hot
         data['too_hot'] = 0
         data.loc[data['delta'] > band, 'too_hot'] = 1
-        data['too_hot'] = data['too_hot'].rolling(window=data.shape[0],min_periods=1).sum()/data.shape[0]
+        data['too_hot'] = data['too_hot'].rolling(window=data.shape[0],min_periods=1).sum()/occupied_time_step_count
 
         # minimum delta
         data['minimum_delta'] = data['delta'].rolling(window=data.shape[0],min_periods=1).min()
