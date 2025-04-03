8800f392f396ae54ebfba3f4515b94b3ab81add4
Rui Pina <1201568@isep.ipp.pt>
2025-04-03 21:58:05 +0100

    dev: readd washing machine work

--- citylearn/citylearn.py
+++ citylearn/citylearn.py
@@ -1707,6 +1707,7 @@ class CityLearnEnv(Environment, Env):
             'cooling_storage': {'autosizer': building.autosize_cooling_storage},
             'heating_storage': {'autosizer': building.autosize_heating_storage},
             'electrical_storage': {'autosizer': building.autosize_electrical_storage},
+            'washing_machine': {'autosizer': building.autosize_electrical_storage},
             'pv': {'autosizer': building.autosize_pv},
 
         }
